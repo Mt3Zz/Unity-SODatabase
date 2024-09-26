08d1ce25be1bc21943baa3b7a0fa2284ab415e8a
administer <ttttt2tttttt+gituser01@gmail.com>
2024-09-26 15:36:51 +0900

    refactor!: Remove CreateAssetMenu from IntObject

--- Assets/SODatabase/Runtime/DataObject/IntObject/IntObject.cs
+++ Assets/SODatabase/Runtime/DataObject/IntObject/IntObject.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 
-
 namespace SODatabase.DataObject
 {
-    [CreateAssetMenu(fileName = "IntObject", menuName = "SODatabase/IntObject")]
     public class IntObject : BaseObject
     {
         public int Value => _value;
         [SerializeField]
         private int _value = 0;
-
     }
 }

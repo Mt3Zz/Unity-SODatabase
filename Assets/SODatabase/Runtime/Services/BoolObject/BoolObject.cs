aa6f3f71773115b3dc7a0f65260976a0cc88a82e
administer <ttttt2tttttt+gituser01@gmail.com>
2024-11-03 18:10:12 +0900

    refactor!: Change BoolObject.Value setter from internal to public
    Add Toggle method

--- Assets/SODatabase/Runtime/Services/BoolObject/BoolObject.cs
+++ Assets/SODatabase/Runtime/Services/BoolObject/BoolObject.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 
-namespace SODatabase.DataObject
+namespace SODatabase
 {
-    public sealed class BoolObject : BaseObject
+    public sealed class BoolObject : DataObject.BaseObject
     {
         public bool Value
         {
-            get { return _value; }
-            internal set { _value = value; }
+            get => _value;
+            set => _value = value;
         }
         [SerializeField]
         private bool _value = false;
+
+
+        public void Toggle()
+        {
+            _value = !_value;
+        }
     }
 }
